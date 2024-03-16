b95085c196801bd766b93ca564f6636d556b9b4b
slava <sg230294@gmail.com>
2024-03-16 12:03:50 +0300

    image modify

--- index.ts
+++ index.ts
@@ -1,12 +1,29 @@
-import {Telegraf} from 'telegraf'
+import {Input, Telegraf} from 'telegraf'
 import * as dotenv from 'dotenv'
 import {message} from "telegraf/filters";
-import * as fs from "node:fs";
+import * as fsAsync from "fs/promises";
+import * as fs from "fs";
 import axios from "axios";
-//import imageDataURI from "image-data-uri";
 import * as imageDataURI from "image-data-uri";
+import * as cheerio from "cheerio";
+import * as convertSvgToPng from 'convert-svg-to-png';
+import * as sharp from 'sharp';
 
 //import template from "-!svg-react-loader!./template.svg"
+async function main() {
+
+  const url = 'https://2ch.hk/b/thumb/266160769/16492546145510s.jpg';
+
+  const response = await axios.get(url,{ responseType: 'arraybuffer' })
+  const buffer = Buffer.from(response.data, "utf-8")
+
+  const image = sharp(buffer);
+  const metadata = await image.metadata();
+  console.log('Width:', metadata.width);
+  console.log('Height:', metadata.height);
+
+
+}
 
 
 dotenv.config();
@@ -18,23 +35,42 @@ bot.on(message('photo'), (ctx) => modifyPicture(ctx));
 async function modifyPicture(ctx: any) {
 
   let imageId = ctx.message.photo.pop().file_id;
+  const url = await ctx.telegram.getFileLink(imageId);
 
-  ctx.telegram.getFileLink(imageId).then((link: string) => {
-    axios({url: link, responseType: 'stream'}).then(response => {
-      return new Promise(() => {
-        response.data.pipe(fs.createWriteStream(`./${imageId}.jpg`))
-            .on('finish', () => console.log('file saved'))
-      });
-    })
-  })
+  const response = await axios.get(url,{ responseType: 'arraybuffer' })
+  const buffer = Buffer.from(response.data, "utf-8")
+  const dataURI = await imageDataURI.encode(buffer, 'PNG');
+
+  const metadata = await sharp(buffer).metadata();
+  const imgWidth = metadata.width;
+  const imgHeight = metadata.height;
+
+
+  //const dataURI = await imageDataURI.encodeFromURL(url);
+  const svgFile = await fsAsync.readFile('./template.svg',"utf-8");
+  const svg = cheerio.load(svgFile, {xmlMode: true});
+  svg('svg.SVG_0').attr('width', imgWidth.toString());
+  svg('svg.SVG_0').attr('height', imgHeight.toString());
+  svg('svg.SVG_0').attr('viewBox', `0 0 ${imgWidth} ${imgHeight}`);
+
+  svg('g.WEIGHT').attr('transform', `matrix(0.5 0 0 0.5 100 ${imgHeight - 100})`);
+
+  svg('image.SVGID_2').prop('href', dataURI);
+  svg('image.SVGID_2').attr('width', imgWidth.toString());
+  svg('image.SVGID_2').attr('height', imgHeight.toString());
+
+  svg('tspan.CAPTION').text(ctx.update.message.caption)
+  // Debug +
+  await fsAsync.writeFile("./result.svg", svg.xml());
+  // Debug -
+  const png = await convertSvgToPng.convert(svg.xml());
+  const photo = Buffer.from(png);
+  ctx.replyWithPhoto(Input.fromBuffer(photo));
+
 
 }
 
-
-
-// example data uri
-imageDataURI.encodeFromURL('https://2ch.hk/b/thumb/266160769/16492546145510s.jpg').then(dataURI => console.log(dataURI))
-
+//main();
 bot.launch();
 console.log('Done!');
 
