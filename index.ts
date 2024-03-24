7eff09538a4a8dcd3a4ff92fde0edb2c5e0a549f
slava <sg230294@gmail.com>
2024-03-24 07:39:10 +0300

    zip and csv parser

--- index.ts
+++ index.ts
@@ -11,28 +11,7 @@ import * as sharp from 'sharp';
 import { parse } from 'csv-parse';
 import * as AdmZip from 'adm-zip'
 
-//import template from "-!svg-react-loader!./template.svg"
 async function main() {
-
-  const processFile = async () => {
-    const records = [];
-    const parser = fs
-        .createReadStream(`./tempCSV.csv`)
-        .pipe(parse({
-          // CSV options if any
-        }));
-    for await (const record of parser) {
-      // Work with each record
-      records.push(record);
-
-    }
-    return records;
-  };
-
-  (async () => {
-    const records = await processFile();
-    console.info(records);
-  })();
 
 }
 
@@ -43,48 +22,34 @@ const bot = new Telegraf(tgToken)
 bot.on(message('photo'), (ctx) => modifyPicture(ctx));
 bot.on(message('document'),(ctx) => processFile(ctx));
 
+
+async function parseFile(csvText) {
+  const parser = await parse(csvText);
+  const records = [];
+  for await (const record of parser) {
+    records.push(record);
+  }
+   return records;
+}
+
 async function processFile(ctx: any) {
 
   const fileId = ctx.update.message.document.file_id;
-
-  ctx.telegram.getFileLink(fileId).then(url => {
-    axios({url, responseType: 'stream'}).then(response => {
-      return new Promise((resolve, reject) => {
-        response.data.pipe(fs.createWriteStream(`tempCSV.csv`))
-            .on('finish', () => console.log('file saved'))
-            .on('error', e => console.log('err'))
-      });
-    })
-  })
-
-  const processFile = async () => {
-    const records = [];
-    const parser = fs
-        .createReadStream(`./tempCSV.csv`)
-        .pipe(parse({
-          // CSV options if any
-        }));
-    for await (const record of parser) {
-      records.push(record);
-    }
-    return records;
-  };
-
+  const url = await ctx.telegram.getFileLink(fileId);
+  const response  = await axios({url, responseType: 'text'})
+  const csvText = response.data;
   let zip = new AdmZip();
-
-  const records = await processFile();
+  const records = await parseFile(csvText);
   let n = 0;
   for (const img of records) {
     const photo = await addOverlay(img[0], img[1]);
-    zip.addFile(`${++n}.png`, photo, 'fvv');
-    //await ctx.replyWithPhoto(Input.fromBuffer(photo));
+    zip.addFile(`${++n}.png`, photo, '');
   }
   // debug +
-  zip.writeZip("files.zip");
+  // zip.writeZip("files.zip");
   // debug -
   let archive = zip.toBuffer();
   await ctx.replyWithDocument({ source: archive , filename: 'photos.zip' });
-
 
 }
 
@@ -110,7 +75,7 @@ async function addOverlay(text: string, url: string){
   svg('image.SVGID_2').attr('height', imgHeight.toString());
   svg('tspan.CAPTION').text(text)
   // Debug +
-  await fsAsync.writeFile("./result.svg", svg.xml());
+  // await fsAsync.writeFile("./result.svg", svg.xml());
   // Debug -
   const png = await convertSvgToPng.convert(svg.xml());
   return Buffer.from(png);
@@ -121,7 +86,6 @@ async function modifyPicture(ctx: any) {
 
   let imageId = ctx.message.photo.pop().file_id;
   const url = await ctx.telegram.getFileLink(imageId);
-
   const photo = await addOverlay(ctx.update.message.caption, url);
   await ctx.replyWithPhoto(Input.fromBuffer(photo));
 
@@ -129,7 +93,7 @@ async function modifyPicture(ctx: any) {
 
 //main();
 bot.launch();
-console.log('Done!');
+console.log('Bot started!');
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'))
